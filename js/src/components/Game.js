305b03e776758ac60f64370231fff64910ffab31
Bobby Saul <bobby.saul@yahoo.com>
2020-11-16 19:46:25 -0500

    Added the base files.

---
+++ js/src/components/Game.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * Loads app.
+ *
+ * @returns {*}
+ * @constructor
+ */
+const Game = () => (
+  <div>
+    The Turkey Hunt Game
+  </div>
+);
+
+export default Game;
